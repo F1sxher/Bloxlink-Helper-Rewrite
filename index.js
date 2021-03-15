ff264c742ae3455e24a913b497cb7e6fc27209a7
Ieuan <31664943+IeuanGol@users.noreply.github.com>
2021-03-15 18:54:15 +0000

    Rewriting Discription because we now use akairo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Import modules
 const Discord = require("discord.js");
-const client = new Discord.Client();
+// const client = new Discord.Client();
 const chalk = require("chalk");
 const figlet = require("figlet");
 const fs = require("fs");
@@ -11,11 +11,42 @@ const colors = require("./colors.js");
 // const tools = require("./tools.js");
 const cooldowns = new Discord.Collection();
 
-// Defining variables
-// client.config = config;
-client.colors = colors;
-// client.tools = tools;
-// to be removed ;)
+const { AkairoClient, CommandHandler, ListenerHandler } = require('discord-akairo');
+
+class MyClient extends AkairoClient {
+    constructor() {
+        super({
+          ownerID: '176490509852737537',// Options for Akairo go here.
+        }, {
+            // Options for discord.js goes here.
+        });
+        this.commandHandler = new CommandHandler(this, {
+          directory: './commands/',
+          prefix: process.env.prefix, // or ['?', '!']
+          aliasReplacement: /-/g,
+          allowMention: true,
+          handleEdits: true,
+          commandUtil: true,
+          argumentDefaults: {
+            prompt: {
+                timeout: 'Command timeout. Prompt cancelled.',
+                ended: 'Too many retries. Prompt cancelled.',
+                cancel: 'Prompt cancelled.',
+                retries: 1,
+                time: 30000
+            }
+           // Options for the command handler goes here.
+        }});
+      this.listenerHandler = new ListenerHandler(this, {
+        directory: './listeners/'
+    });
+    this.commandHandler.loadAll();
+    this.listenerHandler.loadAll();
+    }
+    
+}
+
+const client = new MyClient();
 
 // Setup databases
 const Keyv = require("keyv");
@@ -46,69 +77,5 @@ const tagdb = sequelize.define("tags", {
 });
 tagdb.sync();
 client.tagdb = tagdb;
-
-
-let commandlist = [];
-client.commandlist = commandlist;
-
-fs.readdir('./commands/', async (err, files) => {
-  if(err){
-      return console.log(chalk.red('An error occured when checking the commands folder for commands to load: ' + err));
-  }
-  files.forEach(async (file) => {
-      if(!file.endsWith('.js')) return;
-      let commandFile = require(`./commands/${file}`);
-      commandlist.push({
-        file: commandFile,
-        name: file.split('.')[0],
-        config: commandFile.config
-      });
-  });
-});
-
-client.on('ready', async () => {
-  console.log(chalk.yellow(figlet.textSync('Bot', { horizontalLayout: 'full' })));
-  console.log(chalk.red(`Bot started!\n---\n`
-  + `> Users: ${client.users.cache.size}\n`
-  + `> Channels: ${client.channels.cache.size}\n`
-  + `> Servers: ${client.guilds.cache.size}`));
-  let botstatus = fs.readFileSync('./bot-status.json');
-  botstatus = JSON.parse(botstatus);
-  if(botstatus.activity == 'false') return;
-  if(botstatus.activitytype == 'STREAMING'){
-    client.user.setActivity(botstatus.activitytext, {
-      type: botstatus.activitytype,
-      url: botstatus.activityurl
-    });
-  } else {
-    client.user.setActivity(botstatus.activitytext, {
-      type: botstatus.activitytype
-    });
-  }
-});
-
-client.on('message', async (message) => {
-  if(message.author.bot) return;
-  if(message.channel.type === 'dm') return;
-  
-  if(!message.content.startsWith(process.env.prefix)){
-    if(!message.content.startsWith(`${client.user.id}>`)){
-      return;
-    }
-  }
-  let args = message.content.slice((process.env.prefix).length).split(' ');
-  let commandName = args[0].toLowerCase();
-  args.shift();
-  if(!commandName){
-    commandName = args[0].toLowerCase();
-    args.shift();
-  }
-  let command = commandlist.find((cmd) => cmd.name === commandName);
-  if(!command){
-    command = commandlist.find((cmd) => cmd.config.aliases.includes(commandName));
-    if(!command) return;
-  }
-  command.file.run(client, message, args);
-});
 
 client.login(process.env.token);
