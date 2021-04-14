4eb101f4da7c7b2f9266228c6fe7bf7ed33382ca
F1sxher <72705185+F1sxher@users.noreply.github.com>
2021-04-14 16:17:53 -0400

    duplicate tag-send.js

---
+++ commands/tag/tag-raw.js
@@ -0,0 +1,37 @@
+const { Command } = require('discord-akairo');
+
+// This commands calls tags raw.
+
+class tagsendCommand extends Command {
+    constructor() {
+        super('tag-raw', {
+        //    aliases: ['tag-send'],
+           description: {
+            content: 'Calls tags (raw)',
+            usage: 'tag raw <name>',
+        },
+        });
+    }
+
+    async exec(message) {
+        const args = message.content.slice(process.env.prefix.length).split(' ');
+        if (args[1] === undefined) {
+            return message.channel.send('You have to say what tag you need!')
+        }
+        let tag = await this.client.tagdb.findOne({where: {name: args[1]}})
+        if (!tag) {
+        tag = await this.client.tagdb.findOne({where: {aliases: args[1]}})
+        if (!tag) {
+            try {message.delete({timeout: 1500})} catch(e) {}
+            let msg = await message.channel.send('That tag doesn\'t exist!')
+            return msg.delete({timeout: 1500}) 
+        }
+
+        }
+        message.delete()
+        tag.increment("uses");
+        return message.channel.send(tag.text);
+    }
+}
+
+module.exports = tagsendCommand;
