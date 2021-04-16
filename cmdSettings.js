1b6b5fdce9216417f7ab346cc51741705eff1ee0
Fisher <72705185+F1sxher@users.noreply.github.com>
2021-04-16 08:43:49 -0400

    cmd Settings thing (determines who can sends commands)

---
+++ cmdSettings.js
@@ -0,0 +1,18 @@
+module.exports = {
+  channelExceptions: [ // Allowed Channels
+    '372181138002935819', // #bot-commands
+    '695375776043761744', // #commands (staff channel)
+    '431903222400155658' // #verify
+  ],
+  roleExceptions: [ // Staff Roles (Bypass Commands Warning)
+    '818919735193632858', // Trial Helpers
+    '412791520316358656', // Helpers
+    '372174398918098944', // Mods
+    '539665515430543360', // Developers
+  ],
+  prefixes: [
+    '!', // Bloxlink (or Pro)
+    '!!', // Bloxlink Secondary (If pro)
+    '??' // Bloxlink Canary
+  ]
+}
